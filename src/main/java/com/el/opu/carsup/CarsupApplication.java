4548576875af066b13359085d8c5b400eea57741
themrjoe <vadimmanko666@gmail.com>
2022-03-20 11:29:26 +0200

    Initial commit

---
+++ src/main/java/com/el/opu/carsup/CarsupApplication.java
@@ -0,0 +1,13 @@
+package com.el.opu.carsup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarsupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/el/opu/carsup/CarsupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.el.opu.carsup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
