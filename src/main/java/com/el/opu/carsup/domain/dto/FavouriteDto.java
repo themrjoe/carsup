8b74e2e4d441d82f180ba8ca9a61529a82eaefd2
themrjoe <vadimmanko666@gmail.com>
2022-04-28 19:20:33 +0300

    Fix 4xx error

--- src/main/java/com/el/opu/carsup/domain/dto/FavouriteDto.java
+++ src/main/java/com/el/opu/carsup/domain/dto/FavouriteDto.java
@@ -1,8 +1,12 @@
 package com.el.opu.carsup.domain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FavouriteDto {
 
     private Long idCar;
